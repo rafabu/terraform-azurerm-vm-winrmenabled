22725c5a37e06e67ac2935cd97a913f5eff5bff9
Raphael Burri <raphael.burri@itnetx.ch>
2019-01-09 12:56:24 +0100

    wip

--- main.tf
+++ main.tf
@@ -154,9 +154,9 @@ resource "azurerm_virtual_machine_extension" "script_extension" {
 
   settings = <<SETTINGS_JSON
   {
-    "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File Add-BdeHdCfg.ps1 -bdehdcfgURI ${var.bdehdcfg_uri}",
+    "commandToExecute": "powershell.exe -ExecutionPolicy Unrestricted -File Add-BdeHdCfg.ps1 -bdehdcfgURI \"${var.bdehdcfg_uri}\"",
     "fileUris: ["https://github.com/rafabu/terraform-azurerm-vm-winrmenabled/raw/master/Add-BdeHdCfg.ps1"]
-    "tTimestamp": ""
+    "timestamp": ""
   }
   SETTINGS_JSON
 
