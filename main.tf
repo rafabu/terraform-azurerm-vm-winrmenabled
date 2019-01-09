666d4b25ee34ed0a2d3cd5db3f93510405a565a8
Raphael Burri <raphael.burri@itnetx.ch>
2019-01-09 15:16:07 +0100

    wip

--- main.tf
+++ main.tf
@@ -147,6 +147,7 @@ protected_settings = <<PROTECTED_SETTINGS_JSON
   PROTECTED_SETTINGS_JSON
 }
 resource "azurerm_virtual_machine_extension" "diskencryption_extension_on_core" {
+  #depends on BdeHdCfg_script_extension_on_core
   #count = "${var.keyvault_URL != "" && var.keyvault_resource_id != "" ? 1 : 0}"
   count = 0
   name                 = "AzureDiskEncryption"
@@ -175,7 +176,8 @@ resource "azurerm_virtual_machine_extension" "diskencryption_extension_on_core" 
 }
 resource "azurerm_virtual_machine_extension" "diskencryption_extension_on_gui" {
   #count = "${var.keyvault_URL != "" && var.keyvault_resource_id != "" ? 1 : 0}"
-  count = 0
+  #see that offer != WindowsServer || sku != *.Core
+  count = "${(lookup(var.storage_image_reference, "offer", "") != "WindowsServer" || substr(lookup(var.storage_image_reference, "sku", ""), -4, -1) != "Core") && ${var.keyvault_URL != "" && var.keyvault_resource_id != ""? 1 : 0}"
   name                 = "AzureDiskEncryption"
   location             = "${var.location}"
   resource_group_name  = "${var.resource_group_name}"
